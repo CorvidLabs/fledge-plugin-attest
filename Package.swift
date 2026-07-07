afff3043714fb5e66bf2ca1fc3780a97c0262d19
Leif <leif.algo@pm.me>
2026-07-07 16:51:01 -0600

    Update attest dependency and tag version 1.0.0 (#8)
    * Update attest to 1.0.0 and tag as 1.0.0
    
    - Bump attest dependency in Package.swift to 1.0.0
    - Manually update Package.resolved to point to attest 1.0.0
    - Update fledge-plugin-attest version in plugin.toml to 1.0.0
    - Update README.md to reflect requirement for attest 1.0.0
    
    * Fix CI by adding a dummy test target
    
    - Add a testTarget in Package.swift for fledge-attestTests.
    - Add a dummy test in Tests/fledge-attestTests to satisfy `swift test` run in CI.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .executable(name: "fledge-attest", targets: ["fledge-attest"])
     ],
     dependencies: [
-        .package(url: "https://github.com/CorvidLabs/attest.git", from: "0.5.0"),
+        .package(url: "https://github.com/CorvidLabs/attest.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
     ],
     targets: [
@@ -24,6 +24,10 @@ let package = Package(
             swiftSettings: [
                 .enableExperimentalFeature("StrictConcurrency")
             ]
+        ),
+        .testTarget(
+            name: "fledge-attestTests",
+            dependencies: ["fledge-attest"]
         )
     ]
 )
